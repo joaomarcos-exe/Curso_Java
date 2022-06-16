51a7aeea8fbef8dd102843c53d9e2ec26040e573
joaomarcos <joaomarcos-icm@hotmail.com>
2022-06-16 14:45:26 -0300

    ClassAbstract

---
+++ ClasseAbstratas/src/SerVivo.java
@@ -0,0 +1,3 @@
+public interface SerVivo {
+    public void teste();
+}
---
+++ ClasseAbstratas/src/animal.java
@@ -0,0 +1,6 @@
+//Uma classe abstrata serve apenas para ser herdada e não
+//poder ser instanciada.
+abstract class animal implements SerVivo{
+    public void animal(){}
+    public void teste(){}
+}
