7b696ebea965aff1ba16368de819e2512e22c7a0
joaomarcos <joaomarcos-icm@hotmail.com>
2022-06-19 13:22:39 -0300

    SwitchCase

---
+++ SwitchCase/src/main.java
@@ -0,0 +1,19 @@
+public class main {
+
+    public static void main(String[] args){
+        int position = 1;
+
+        switch(position){
+            case 1:
+                System.out.println("E o valor 1");
+                break;
+            case 2:
+                System.out.println("O valor e 2");
+                break;
+            default:
+                System.out.println("Sem valor");
+                break;
+        }
+    }
+
+}
